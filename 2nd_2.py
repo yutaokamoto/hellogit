ce2f5ba722ffba87a3f78f63e719802d7f7b5fec
岡本優太 <okamoto@okamotoyuutanoMacBook-puro.local>
2019-02-21 13:10:08 +0900

    add new file!!

---
+++ 2nd_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('これがGitHubの二番めのファイル')
+
+
+# In[2]:
+
+
+n = int(input('n = '))
+
+
+# In[3]:
+
+
+for i in range(n):print(i)
+
+
+# In[ ]:
+
+
+
+
